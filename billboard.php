d0e62799328dbc7c449b36b184b2bf59c0fcf30d
20181150 <20181150@mita-is.ed.jp>
2021-09-27 17:00:12 +0900

    indent

--- billboard.php
+++ billboard.php
@@ -13,19 +13,19 @@
     </form>
     <?php
     // $fileにmessages.txtを定義
-   $file='messages.txt' ;
+    $file='messages.txt' ;
    // ファイルが存在する時のみ$fileをデコードした文字列を$boardに保存
-   if (file_exists($file)) {
-       $board=json_decode(file_get_contents($file));
-   }
-   // $boardにmessageのデータを追加
-   $board[]=$_REQUEST['message'];
-   // $fileに$boardのデータをエンコードした文字列を追加
-   file_put_contents($file, json_encode($board));
-   // 表示の際に全てのメッセージを表示させる
-   foreach ($board as $message) {
-       echo '<p>', $message, '</p><hr>';
-   }
-   ?>
+    if (file_exists($file)) {
+        $board=json_decode(file_get_contents($file));
+    }
+    // $boardにmessageのデータを追加
+    $board[]=$_REQUEST['message'];
+    // $fileに$boardのデータをエンコードした文字列を追加
+    file_put_contents($file, json_encode($board));
+    // 表示の際に全てのメッセージを表示させる
+    foreach ($board as $message) {
+        echo '<p>', $message, '</p><hr>';
+    }
+    ?>
 </body>
 </html>
