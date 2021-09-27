d0cd4cf6eb27e9cf3373b6c26adcf0b5d4b59835
20181150 <20181150@mita-is.ed.jp>
2021-09-27 17:13:51 +0900

    indent

--- billboard.php
+++ billboard.php
@@ -1,7 +1,7 @@
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Billboard</title>
+<head>
+    <meta charset="utf-8">
+    <title>Billboard</title>
 </head>
 <body>
     <h1>掲示板</h1>
@@ -12,20 +12,20 @@
         <input type="submit" value="投稿">
     </form>
     <?php
-    // $fileにmessages.txtを定義
-    $file='messages.txt' ;
-    // ファイルが存在する時のみ$fileをデコードした文字列を$boardに保存
-    if (file_exists($file)) {
-        $board=json_decode(file_get_contents($file));
-    }
-    // $boardにmessageのデータを追加
-    $board[]=$_REQUEST['message'];
-    // $fileに$boardのデータをエンコードした文字列を追加
-    file_put_contents($file, json_encode($board));
-    // 表示の際に全てのメッセージを表示させる
-    foreach ($board as $message) {
-        echo '<p>', $message, '</p><hr>';
-    }
+        // $fileにmessages.txtを定義
+        $file='messages.txt' ;
+        // ファイルが存在する時のみ$fileをデコードした文字列を$boardに保存
+        if (file_exists($file)) {
+            $board=json_decode(file_get_contents($file));
+        }
+        // $boardにmessageのデータを追加
+        $board[]=$_REQUEST['message'];
+        // $fileに$boardのデータをエンコードした文字列を追加
+        file_put_contents($file, json_encode($board));
+        // 表示の際に全てのメッセージを表示させる
+        foreach ($board as $message) {
+            echo '<p>', $message, '</p><hr>';
+        }
     ?>
 </body>
 </html>
